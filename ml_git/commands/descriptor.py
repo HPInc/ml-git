18fcbf99b1b343633dde92cd87d7a1d5992fb57f
Lucas <lucas.cabral@ccc.ufcg.edu.br>
2022-02-22 11:24:11 -0300

    Fixing descriptor adding full to checkout command

--- ml_git/commands/descriptor.py
+++ ml_git/commands/descriptor.py
@@ -85,7 +85,7 @@ commands = [
             '--bare': {'default': False, 'is_flag': True, 'help': help_msg.BARE_OPTION},
             '--version': {'default': -1, 'help': help_msg.ARTIFACT_VERSION},
             '--fail-limit': {'type': int, 'help': help_msg.FAIL_LIMIT},
-            '--full': {'is_flag': True, 'default': False, 'help': help_msg.STATUS_FULL_OPTION},
+            '--full': {'is_flag': True, 'default': False, 'help': help_msg.STATUS_FULL_OPTION}
         },
 
         'arguments': {
@@ -114,7 +114,8 @@ commands = [
             '--force': {'is_flag': True, 'default': False, 'help': help_msg.FORCE_CHECKOUT},
             '--bare': {'default': False, 'is_flag': True, 'help': help_msg.BARE_OPTION},
             '--version': {'default': -1, 'help': help_msg.ARTIFACT_VERSION},
-            '--fail-limit': {'type': int, 'help': help_msg.FAIL_LIMIT}
+            '--fail-limit': {'type': int, 'help': help_msg.FAIL_LIMIT},
+            '--full': {'is_flag': True, 'default': False, 'help': help_msg.STATUS_FULL_OPTION}
         },
 
         'help': 'Checkout the ML_ENTITY_TAG|ML_ENTITY of a label set into user workspace.'
@@ -133,7 +134,8 @@ commands = [
             '--force': {'default': False, 'is_flag': True, 'help': help_msg.FORCE_CHECKOUT},
             '--bare': {'default': False, 'is_flag': True, 'help': help_msg.BARE_OPTION},
             '--version': {'default': -1, 'help': help_msg.ARTIFACT_VERSION},
-            '--fail-limit': {'type': int, 'help': help_msg.FAIL_LIMIT}
+            '--fail-limit': {'type': int, 'help': help_msg.FAIL_LIMIT},
+            '--full': {'is_flag': True, 'default': False, 'help': help_msg.STATUS_FULL_OPTION}
         },
 
         'arguments': {
