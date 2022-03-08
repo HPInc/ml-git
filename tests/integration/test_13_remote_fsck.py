f482f18a2b8ac02e41db59bd3ab798bea7ed5b71
VieiraCarlos <carlos.artur.n@gmail.com>
2022-03-08 15:55:31 -0300

    Changing the log level for Corruption Detected messages on the remote fsck --paranoid command

--- tests/integration/test_13_remote_fsck.py
+++ tests/integration/test_13_remote_fsck.py
@@ -78,7 +78,7 @@ class RemoteFsckAcceptanceTests(unittest.TestCase):
 
         output = check_output(MLGIT_REMOTE_FSCK % (DATASETS, DATASET_NAME) + ' --paranoid')
 
-        self.assertIn(output_messages['ERROR_CORRUPTION_DETECTED_FOR'] % self.file, output)
+        self.assertNotIn(output_messages['ERROR_CORRUPTION_DETECTED_FOR'] % self.file, output)  # msg is debug-only
         self.assertIn(output_messages['INFO_REMOTE_FSCK_FIXED'] % (1, 0), output)
 
         self.assertNotIn(output_messages['ERROR_CORRUPTION_DETECTED_FOR'], check_output(MLGIT_REMOTE_FSCK % (DATASETS, DATASET_NAME) + ' --paranoid'))
