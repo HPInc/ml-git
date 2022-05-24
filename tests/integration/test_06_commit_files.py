9c33ba5d828ff355cde3a21bd9fc8658cde213df
MatheusClaudi <matheus.claudino@ccc.ufcg.edu.br>
2022-05-24 00:33:11 -0300

    Adding testes for commit wizard

--- tests/integration/test_06_commit_files.py
+++ tests/integration/test_06_commit_files.py
@@ -7,8 +7,10 @@ import os
 import unittest
 
 import pytest
+from click.testing import CliRunner
 
 from ml_git.constants import MLGIT_IGNORE_FILE_NAME
+from ml_git.commands import entity, help_msg
 from ml_git.ml_git_message import output_messages
 from tests.integration.commands import MLGIT_COMMIT, MLGIT_ADD
 from tests.integration.helper import check_output, add_file, ML_GIT_DIR, entity_init, create_spec, create_file, \
@@ -129,3 +131,24 @@ class CommitFilesAcceptanceTests(unittest.TestCase):
         ignore_file = os.path.join(metadata, MLGIT_IGNORE_FILE_NAME)
         self.assertTrue(os.path.exists(ignore_file))
         self.assertTrue(os.path.exists(manifest_file))
+
+    @pytest.mark.usefixtures('start_local_git_server', 'switch_to_tmp_dir')
+    def test_08_commit_files_to_labels_with_wizard_enabled(self):
+        entity_type = LABELS
+        entity_init(entity_type, self)
+        add_file(self, entity_type, '--bumpversion', 'new')
+        runner = CliRunner()
+        # These parameters bellow ENTITY_NAME and LABEL_USER_INPUT not reference real entity in the system (Only for test)
+        result = runner.invoke(entity.labels, ['commit', 'ENTITY_NAME'], input='LABEL_USER_INPUT\n')
+        self.assertIn(help_msg.LINK_DATASET, result.output)
+        
+    @pytest.mark.usefixtures('start_local_git_server', 'switch_to_tmp_dir')
+    def test_09_commit_files_to_labels_with_wizard_enabled(self):
+        entity_type = MODELS
+        entity_init(entity_type, self)
+        add_file(self, entity_type, '--bumpversion', 'new')        
+        runner = CliRunner()
+        # These parameters bellow ENTITY_NAME and LABEL_USER_INPUT not reference real entity in the system (Only for test)
+        result = runner.invoke(entity.models, ['commit', 'ENTITY_NAME'], input='DATASET_USER_INPUT\nLABEL_USER_INPUT')
+        self.assertIn(help_msg.LINK_DATASET, result.output)
+        self.assertIn(help_msg.LINK_LABELS, result.output)
