f4122735f7232f8b90c646dcd0320e558a42bec0
Lucas <lucas.cabral@ccc.ufcg.edu.br>
2022-06-15 11:45:14 -0300

    Adding method to validate the input in the wizard-config option of create

--- ml_git/config.py
+++ ml_git/config.py
@@ -1,5 +1,5 @@
 """
-© Copyright 2020-2021 HP Development Company, L.P.
+© Copyright 2020-2022 HP Development Company, L.P.
 SPDX-License-Identifier: GPL-2.0-only
 """
 
@@ -9,7 +9,7 @@ from pathlib import Path
 
 from halo import Halo
 
-from ml_git import spec
+from ml_git import spec, log
 from ml_git.constants import FAKE_STORAGE, BATCH_SIZE_VALUE, BATCH_SIZE, StorageType, GLOBAL_ML_GIT_CONFIG, \
     PUSH_THREADS_COUNT, SPEC_EXTENSION, EntityType, STORAGE_CONFIG_KEY, STORAGE_SPEC_KEY, DATASET_SPEC_KEY, \
     MultihashStorageType
@@ -351,6 +351,17 @@ def _configure_metadata_remote(repo_type):
         remote_add(repo_type, git_repo)
 
 
+def _get_user_input(message, default=None, required=False):
+    value = input(message)
+    if not value:
+        if required:
+            log.warn(output_messages['ERROR_EMPTY_VALUE'])
+            return _get_user_input(message, default, required)
+        else:
+            return default
+    return value
+
+
 def _create_new_bucket():
     storages_types = [item.value for item in MultihashStorageType]
     storage_type = input(USER_INPUT_MESSAGE.format('storage type {}'.format(str(storages_types)))).lower()
@@ -361,17 +372,17 @@ def _create_new_bucket():
 
     if storage_type not in storages_types:
         raise RuntimeError(output_messages['ERROR_INVALID_STORAGE_TYPE'])
-    bucket = input(USER_INPUT_MESSAGE.format('bucket name'))
+    bucket = _get_user_input(USER_INPUT_MESSAGE.format('bucket name'), required=True)
     if storage_type == StorageType.S3H.value:
-        credential_profile = input(USER_INPUT_MESSAGE.format('credentials'))
-        endpoint = input('If you are using MinIO inform the endpoint URL, otherwise press ENTER: ')
+        credential_profile = _get_user_input(USER_INPUT_MESSAGE.format('credentials'))
+        endpoint = _get_user_input('If you are using MinIO inform the endpoint URL, otherwise press ENTER: ')
     elif storage_type == StorageType.GDRIVEH.value:
-        credential_profile = input(USER_INPUT_MESSAGE.format('credentials path'))
+        credential_profile = _get_user_input(USER_INPUT_MESSAGE.format('credentials path'), default='.')
     elif storage_type == StorageType.SFTPH.value:
-        endpoint = input(USER_INPUT_MESSAGE.format('endpoint URL'))
-        sftp_configs = {'username': input(USER_INPUT_MESSAGE.format('username')),
-                        'private_key': input(USER_INPUT_MESSAGE.format('credentials path')),
-                        'port': int(input(USER_INPUT_MESSAGE.format('port')))}
+        endpoint = _get_user_input(USER_INPUT_MESSAGE.format('endpoint URL'))
+        sftp_configs = {'username': _get_user_input(USER_INPUT_MESSAGE.format('username')),
+                        'private_key': _get_user_input(USER_INPUT_MESSAGE.format('credentials path'), default='.'),
+                        'port': int(_get_user_input(USER_INPUT_MESSAGE.format('port'), default=22))}
     from ml_git.admin import storage_add
     storage_add(storage_type, bucket, credential_profile, endpoint_url=endpoint, sftp_configs=sftp_configs)
     return storage_type, bucket
