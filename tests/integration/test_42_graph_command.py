7be55bf452be30732616b21eb5cf93cf2e8c731b
Lucas <lucas.cabral@ccc.ufcg.edu.br>
2022-08-22 17:11:16 -0300

    Fixing integration test

--- tests/integration/test_42_graph_command.py
+++ tests/integration/test_42_graph_command.py
@@ -16,54 +16,52 @@ from tests.integration.helper import check_output, init_repository, DATASETS, DA
 
 @pytest.mark.usefixtures('tmp_dir')
 class GraphCommandsAcceptanceTests(unittest.TestCase):
+    def create_file_in_ws(self, entity, name, value):
+        with open(os.path.join(entity, entity + '-ex', name), 'wt') as z:
+            z.write(value * 100)
 
-    def init_entities_with_relationships(self, only_dataset=False):
+    @pytest.mark.usefixtures('start_local_git_server', 'switch_to_tmp_dir')
+    def test_01_graph_command(self):
         init_repository(DATASETS, self)
         self.create_file_in_ws(DATASETS, 'file', '1')
-        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_ADD % (DATASETS, DATASETS, 'file --bumpversion')))
-        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_COMMIT % (DATASETS, DATASETS, '')))
+        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_ADD % (DATASETS, DATASET_NAME, 'file --bumpversion')))
+        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_COMMIT % (DATASETS, DATASET_NAME, '')))
         head = os.path.join(self.tmp_dir, ML_GIT_DIR, DATASETS, 'refs', DATASET_NAME, 'HEAD')
         self.assertTrue(os.path.exists(head))
 
         tag = 'computer-vision__images__{}__1'
-        if not only_dataset:
-            label_name = 'labels-ex'
-            init_repository(LABELS, self)
-            self.create_file_in_ws(LABELS, 'file', '0')
-            self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_ADD % (LABELS, label_name, 'file --bumpversion')))
-            self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_COMMIT % (LABELS, label_name, '')))
-            labels_metadata = os.path.join(self.tmp_dir, ML_GIT_DIR, LABELS, 'metadata')
-            with open(os.path.join(labels_metadata, label_name, '{}.spec'.format(label_name))) as y:
-                spec = yaml_processor.load(y)
-            head = os.path.join(self.tmp_dir, ML_GIT_DIR, LABELS, 'refs', label_name, 'HEAD')
 
-            self.assertTrue(os.path.exists(head))
-            self.assertEqual(tag.format(DATASET_NAME), spec[LABELS_SPEC_KEY][DATASET_SPEC_KEY]['tag'])
+        label_name = 'labels-ex'
+        init_repository(LABELS, self)
+        self.create_file_in_ws(LABELS, 'file', '0')
+        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_ADD % (LABELS, label_name, 'file --bumpversion')))
+        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_COMMIT % (LABELS, label_name, '--dataset=dataset-ex')))
+        labels_metadata = os.path.join(self.tmp_dir, ML_GIT_DIR, LABELS, 'metadata')
+        with open(os.path.join(labels_metadata, label_name, '{}.spec'.format(label_name))) as y:
+            spec = yaml_processor.load(y)
+        head = os.path.join(self.tmp_dir, ML_GIT_DIR, LABELS, 'refs', label_name, 'HEAD')
 
-            model_name = 'models-ex'
-            init_repository(MODELS, self)
-            self.create_file_in_ws(MODELS, 'file', '0')
-            self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_ADD % (MODELS, model_name, 'file --bumpversion')))
-            self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_COMMIT % (MODELS, model_name, '')))
+        self.assertTrue(os.path.exists(head))
+        self.assertEqual(tag.format(DATASET_NAME), spec[LABELS_SPEC_KEY][DATASET_SPEC_KEY]['tag'])
 
-            self.create_file_in_ws(MODELS, 'file2', '2')
-            self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_ADD % (MODELS, model_name, 'file2 --bumpversion')))
-            self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_COMMIT % (MODELS, model_name, '')))
+        model_name = 'models-ex'
+        init_repository(MODELS, self)
+        self.create_file_in_ws(MODELS, 'file', '0')
+        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_ADD % (MODELS, model_name, 'file --bumpversion')))
+        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_COMMIT % (MODELS, model_name, '')))
 
-            models_metadata = os.path.join(self.tmp_dir, ML_GIT_DIR, MODELS, 'metadata')
-            with open(os.path.join(models_metadata, model_name, '{}.spec'.format(model_name))) as y:
-                spec = yaml_processor.load(y)
-            head = os.path.join(self.tmp_dir, ML_GIT_DIR, MODELS, 'refs', model_name, 'HEAD')
+        self.create_file_in_ws(MODELS, 'file2', '2')
+        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_ADD % (MODELS, model_name, 'file2 --bumpversion')))
+        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_COMMIT % (MODELS, model_name, '--dataset=dataset-ex --labels=labels-ex')))
 
-            self.assertTrue(os.path.exists(head))
-            self.assertEqual(tag.format(DATASET_NAME), spec[MODEL_SPEC_KEY][DATASET_SPEC_KEY]['tag'])
-            self.assertEqual(tag.format(label_name), spec[MODEL_SPEC_KEY][LABELS_SPEC_KEY]['tag'])
+        models_metadata = os.path.join(self.tmp_dir, ML_GIT_DIR, MODELS, 'metadata')
+        with open(os.path.join(models_metadata, model_name, '{}.spec'.format(model_name))) as y:
+            spec = yaml_processor.load(y)
+        head = os.path.join(self.tmp_dir, ML_GIT_DIR, MODELS, 'refs', model_name, 'HEAD')
 
-    @pytest.mark.usefixtures('start_local_git_server', 'switch_to_tmp_dir')
-    def test_01_graph_command(self):
-        self.init_entities_with_relationships()
-        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_GRAPH.format('--export-path=')))
-        self.assertTrue(os.path.exists('entities_relationships.html'))
+        self.assertTrue(os.path.exists(head))
+        self.assertEqual(tag.format(DATASET_NAME), spec[MODEL_SPEC_KEY][DATASET_SPEC_KEY]['tag'])
+        self.assertEqual(tag.format(label_name), spec[MODEL_SPEC_KEY][LABELS_SPEC_KEY]['tag'])
 
     @pytest.mark.usefixtures('start_local_git_server', 'switch_to_tmp_dir')
     def test_02_graph_with_empty_export_path(self):
