cd7e6d7f539891fe5a6903c9ccdc37a54036bba2
Lucas <lucas.cabral@ccc.ufcg.edu.br>
2022-09-23 15:04:32 -0300

    Fixing integration test

--- tests/integration/test_43_get_command.py
+++ tests/integration/test_43_get_command.py
@@ -13,7 +13,7 @@ from tests.integration.commands import MLGIT_COMMIT, MLGIT_PUSH, MLGIT_GET, MLGI
 from tests.integration.helper import check_output, init_repository, DATASETS, DATASET_NAME, ERROR_MESSAGE, add_file, \
     create_git_clone_repo, PATH_TEST
 
-file = 'test_file'
+file = 'file0'
 
 
 @pytest.mark.usefixtures('tmp_dir')
@@ -21,56 +21,47 @@ class GetCommandsAcceptanceTests(unittest.TestCase):
 
     def _setup_get_command(self, entity=DATASETS):
         init_repository(entity, self)
-        add_file(self, entity, '', file)
-        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_COMMIT % (entity, entity + '-ex', '--version=2')))
-        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_PUSH % (entity, entity + '-ex')))
+        add_file(self, entity, '', artifact_name=DATASET_NAME)
+        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_COMMIT % (entity, DATASET_NAME, '--version=2')))
+        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_PUSH % (entity, DATASET_NAME)))
 
     @pytest.mark.usefixtures('start_local_git_server', 'switch_to_tmp_dir')
     def test_01_get_command(self):
         self._setup_get_command()
-        self.assertIn(output_messages['INFO_SUCCESSFULLY_MOUNTED_FILE'], check_output(MLGIT_GET % (DATASETS, DATASET_NAME, file)))
+        self.assertIn(output_messages['INFO_SUCCESSFULLY_MOUNTED_FILE'], check_output(MLGIT_GET.format(DATASETS, DATASET_NAME, file)))
         self.assertTrue(os.path.exists(file))
 
     @pytest.mark.usefixtures('start_local_git_server', 'switch_to_tmp_dir')
     def test_02_get_command_on_the_fly(self):
         GIT_CLONE = os.path.join(PATH_TEST, 'git_clone.git')
-        os.makedirs(self.GIT_CLONE, exist_ok=True)
+        os.makedirs(GIT_CLONE, exist_ok=True)
         create_git_clone_repo(GIT_CLONE, self.tmp_dir)
         self._setup_get_command()
-        self.assertIn(output_messages['INFO_SUCCESSFULLY_MOUNTED_FILE'], check_output(MLGIT_GET_ON_THE_FLY % (DATASETS, DATASET_NAME, file, GIT_CLONE)))
+        self.assertIn(output_messages['INFO_GETTING_FILES'], check_output(MLGIT_GET_ON_THE_FLY.format(DATASETS, DATASET_NAME, file, GIT_CLONE)))
 
     @pytest.mark.usefixtures('start_local_git_server', 'switch_to_tmp_dir')
     def test_03_get_invalid_file(self):
         self._setup_get_command()
         wrong_file = 'wrong_file'
-        self.assertIn(output_messages['ERROR_COULD_NOT_FIND_FILE'].format(wrong_file, DATASET_NAME),
-                      check_output(MLGIT_GET % (DATASETS, DATASET_NAME, wrong_file)))
+        self.assertIn(output_messages['ERROR_INVALID_FILE'].format(wrong_file, DATASET_NAME),
+                      check_output(MLGIT_GET.format(DATASETS, DATASET_NAME, wrong_file)))
         self.assertFalse(os.path.exists(file))
         self.assertFalse(os.path.exists(wrong_file))
 
     @pytest.mark.usefixtures('start_local_git_server', 'switch_to_tmp_dir')
     def test_04_get_unintialized_dir(self):
-        self.assertIn(output_messages['ERROR_NOT_IN_RESPOSITORY'], check_output(MLGIT_GET % (DATASETS, DATASET_NAME, file)))
+        self.assertIn(output_messages['ERROR_NOT_IN_RESPOSITORY'], check_output(MLGIT_GET.format(DATASETS, DATASET_NAME, file)))
         self.assertFalse(os.path.exists(file))
 
     @pytest.mark.usefixtures('start_local_git_server', 'switch_to_tmp_dir')
-    def test_05_get_with_invalid_git_url(self):
-        GIT_CLONE = os.path.join(PATH_TEST, 'wrong_git_clone.git')
-        os.makedirs(self.GIT_CLONE, exist_ok=True)
-        create_git_clone_repo(GIT_CLONE, self.tmp_dir)
+    def test_05_get_with_invalid_entity_name(self):
         self._setup_get_command()
-        self.assertIn(output_messages['INFO_NOT_READY_REMOTE_REPOSITORY'], check_output(MLGIT_GET_ON_THE_FLY % (DATASETS, DATASET_NAME, file, GIT_CLONE)))
+        self.assertIn(output_messages['ERROR_WITHOUT_TAG_FOR_THIS_ENTITY'], check_output(MLGIT_GET.format(DATASETS, 'wrong_name', file)))
         self.assertFalse(os.path.exists(file))
 
     @pytest.mark.usefixtures('start_local_git_server', 'switch_to_tmp_dir')
-    def test_06_get_with_invalid_entity_name(self):
-        self._setup_get_command()
-        self.assertIn(output_messages['ERROR_WITHOUT_TAG_FOR_THIS_ENTITY'], check_output(MLGIT_GET % (DATASETS, 'wrong_name', file)))
-        self.assertFalse(os.path.exists(file))
-
-    @pytest.mark.usefixtures('start_local_git_server', 'switch_to_tmp_dir')
-    def test_07_get_with_invalid_entity_version(self):
+    def test_06_get_with_invalid_entity_version(self):
         self._setup_get_command()
         self.assertIn(output_messages['ERROR_WRONG_VERSION_NUMBER_TO_CHECKOUT'].format(''),
-                      check_output(MLGIT_GET % (DATASETS, DATASET_NAME, file)))
+                      check_output(MLGIT_GET.format(DATASETS, DATASET_NAME, file + ' --version=100')))
         self.assertFalse(os.path.exists(file))
