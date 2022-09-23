c13c97b2d7f7a63f4347436a3b56abc9732e315d
MatheusClaudi <51119782+MatheusClaudi@users.noreply.github.com>
2022-09-23 16:08:28 -0300

    Fixing issue with checkout version when repository connection isn't available (#273)
    * Fixing connection issue with command checkout

--- ml_git/repository.py
+++ ml_git/repository.py
@@ -994,7 +994,10 @@ class Repository(object):
 
         try:
             r = LocalRepository(self.__config, objects_path, repo_type)
-            r.checkout(cache_path, metadata_path, ws_path, tag, samples, bare, entity_dir, options['fail_limit'])
+            sucess = r.checkout(cache_path, metadata_path, ws_path, tag, samples, bare, entity_dir, options['fail_limit'])
+            # If something fail will not save version data
+            if not sucess:
+                return None, None
         except OSError as e:
             self._checkout_ref()
             if e.errno == errno.ENOSPC:
